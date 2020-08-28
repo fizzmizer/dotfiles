bf8fe66723049b8e489426e10a9bf2b99786fbc2
fizzmizer <fizzmizer@mailoo.org>
2020-08-28 08:07:15 +0200

    remove dwm (move to new repo)

--- dotfiles/dwm/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#wal -i ~/Pictures/Comics\ and\ wallpapers/wallpapers/ &
-feh --bg-fill --randomize  ~/Pictures/Comics\ and\ wallpapers/wallpapers/*
-xmodmap ~/dotfiles/xmodmap &
-picom -f --config ~/.config/picom.conf &
-dwmblocks &
-nextcloud --background &
