7843d79f663faf4428ca7c1dcd65241b574b377b
Antoine <fizzmizer@mailoo.org>
2024-11-04 14:10:32 +0100

    nettoyage

---
+++ dwm/autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#wal -i ~/Pictures/Comics\ and\ wallpapers/wallpapers/ &
+#feh --bg-fill --randomize  ~/Pictures/Comics\ and\ wallpapers/wallpapers/*
+feh --bg-fill --randomize  ~/Pictures/Comics\ and\ wallpapers/wallpapers/favs/*
+#conky -c ~/.config/conky/Pleione/Pleione.conf &
+xmodmap ~/dotfiles/xmodmap &
+picom -f --config ~/.config/picom.conf &
+#dwmblocks &
+#nextcloud --background &
+nm-applet &
+#lxqt-policykit-agent &
+#element-desktop &
+#notion-app-enhanced &
