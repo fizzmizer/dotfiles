a85345b63b7f716ece69e4c75cc7a795ce80e59f
Antoine <fizzmizer@mailoo.org>
2024-11-08 11:37:23 +0100

    petites mises à jour, changement de wlogout

--- hyprland/.config/hypr/script/wlogout-launch.sh
+++ hyprland/.config/hypr/script/wlogout-launch.sh
@@ -16,4 +16,5 @@ hypr_scale=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .scale'
 #// eval config files
 wStyle="$(envsubst < $wlTmplt)"
 
-wlogout --layout "${wLayout}" --css <(echo "${wStyle}") --protocol layer-shell -T $(awk "BEGIN {printf \"%.0f\", $A_1080 * 1080 * $hypr_scale / $resolution}") -B $(awk "BEGIN {printf \"%.0f\", $B_1080 * 1080 * $hypr_scale / $resolution}") &
+#wlogout --layout "${wLayout}" --css <(echo "${wStyle}") --protocol layer-shell -T $(awk "BEGIN {printf \"%.0f\", $A_1080 * 1080 * $hypr_scale / $resolution}") -B $(awk "BEGIN {printf \"%.0f\", $B_1080 * 1080 * $hypr_scale / $resolution}") &
+wlogout --layout "${wLayout}" --css <(echo "${wStyle}") &
